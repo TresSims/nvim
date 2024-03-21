71941e4a689d304779110280efc54a77e4e280dd
Tres Sims <tres.sims95@gmail.com>
2024-03-21 03:25:18 +0000

    Update tree to use window picker by default

--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -1,12 +1,25 @@
 -- add window picker
 
 return {
-  {
-    "s1n7ax/nvim-window-picker",
-    name = "window-picker",
-    event = "VeryLazy",
-    config = function()
-      require("window-picker").setup()
-    end,
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["<cr>"] = "open_with_window_picker",
+      },
+    },
+  },
+  dependencies = {
+    {
+      "s1n7ax/nvim-window-picker",
+      name = "window-picker",
+      event = "VeryLazy",
+      config = function()
+        require("window-picker").setup()
+      end,
+      opts = {
+        selection_chars = "FJDKSLA;",
+      },
+    },
   },
 }
