fe2b860c996ec4e1859dce36c53a4fb3bf0f0d29
Tres Sims <tres.sim95@gmail.com>
2024-05-02 11:21:59 -0400

    Added tabpage navigation keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,7 @@ map("n", "<leader>wj", "<C-W>j", { desc = "Go to the down window" })
 map("n", "<leader>wh", "<C-W>h", { desc = "Go to the left window" })
 map("n", "<leader>wk", "<C-W>k", { desc = "Go to the up window" })
 map("n", "<leader>wl", "<C-W>l", { desc = "Go to the right window" })
+
+-- Additional tab navigation
+map("n", "<leader><tab>j", ":tabn<CR>", { desc = "Go to next tabpage" })
+map("n", "<leader><tab>k", ":tabp<CR>", { desc = "Go to previous tabpage" })
